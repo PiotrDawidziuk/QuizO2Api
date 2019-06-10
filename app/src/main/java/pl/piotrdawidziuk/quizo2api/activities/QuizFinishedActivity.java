38486b3b8c4571d458224aac3df1aecc0cfeb165
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-06-10 15:55:56 +0200

    points gained and max points

--- app/src/main/java/pl/piotrdawidziuk/quizo2api/activities/QuizFinishedActivity.java
+++ app/src/main/java/pl/piotrdawidziuk/quizo2api/activities/QuizFinishedActivity.java
@@ -10,18 +10,23 @@ import pl.piotrdawidziuk.quizo2api.R;
 public class QuizFinishedActivity extends AppCompatActivity {
 
     TextView pointsMaxTextView;
+    TextView pointsGainedTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_finished);
 
         pointsMaxTextView = findViewById(R.id.quiz_finished_points_max);
+        pointsGainedTextView = findViewById(R.id.quiz_finished_points_gained);
 
         Intent intent = getIntent();
         int pointsMax = 0;
+        int pointsGained =0;
         pointsMax = intent.getIntExtra("points_max",0);
+        pointsGained = intent.getIntExtra("point_gained",0);
 
-        pointsMaxTextView.setText(""+pointsMax);
+        pointsMaxTextView.setText("Max points: "+pointsMax);
+        pointsGainedTextView.setText("You got: "+pointsGained);
 
 
     }
